51dd9dd681cecb009da496d3c7685e39412f0e1a
frenchie916 <frances.acevedo2@upr.edu>
2013-11-15 19:54:36 -0400

    New Stuff

---
+++ appjs/cart.js
@@ -0,0 +1,7 @@
+module.exports =  { 
+        Cart : function (productID, userID){
+                this.cartId = "";
+                this.productID = productID;
+                this.userID = userID;
+        }
+};
