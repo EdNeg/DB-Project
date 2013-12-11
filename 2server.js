834dacd8d6af2ed6c13da734b76171b2d6875826
Edwin <edwin.negron3@upr.edu>
2013-12-11 19:47:04 -0400

    added postgres connection

--- 2server.js
+++ 2server.js
@@ -30,7 +30,7 @@ app.configure(function () {
 app.use(express.bodyParser());
 
 // Server starts running when listen is called.
-var port = process.env.PORT || 3412;
+var port = process.env.PORT || 5432;
 process.env.PWD = process.cwd();
 app.use(express.static(process.env.PWD));
 app.listen(port, function(){
@@ -68,13 +68,23 @@ var connection = mysql.createConnection({
 });*/
 
 // Postgres Database connection string: pg://<username>:<password>@host:port/dbname 
+
+var conString = "postgres://rgogqzpjvbmvuq:8AfsdO0anC3CJQz0BfD67e7fbS@ec2-54-225-103-9.compute-1.amazonaws.com:5432/d3m3opu022njhi";
+
+var connection = new pg.Client(conString);
+connection.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+/*
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   client.query('SELECT * FROM your_table', function(err, result) {
     done();
     if(err) return console.error(err);
     console.log(result.rows);
   });
-});
+});*/
+
 
 
 
