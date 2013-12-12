a9d1a8d2e4b1c54aa0c6bf3d43cc7183687032ba
Edwin <edwin.negron3@upr.edu>
2013-12-11 20:09:16 -0400

    another

--- 2server.js
+++ 2server.js
@@ -69,20 +69,24 @@ var connection = mysql.createConnection({
 
 // Postgres Database connection string: pg://<username>:<password>@host:port/dbname 
 
-var conString = "postgres://rgogqzpjvbmvuq:8AfsdO0anC3CJQz0BfD67e7fbS@ec2-54-225-103-9.compute-1.amazonaws.com:5432/d3m3opu022njhi";
+/*
+var conString = "postgres://rgogqzpjvbmvuq:8AfsdO0anC3CJQz0BfD67e7fbS@ec2-54-225-103-9.compute-1.amazonaws.com/d3m3opu022njhi";
 
 var connection = new pg.Client(conString);
 connection.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
-  }
-/*
+  }*/
+
+  
+  
+
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM your_table', function(err, result) {
+  client.query('SELECT * FROM bbProduct', function(err, result) {
     done();
     if(err) return console.error(err);
     console.log(result.rows);
-  });*/
+  });
 });
 
 
